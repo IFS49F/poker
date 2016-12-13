a1052e5e6a90de1dea982cacc54706564450b0b7
Hiveer <ljp.hiveer@gmail.com>
2016-12-13 18:32:55 +0800

    debug window.location assign

--- javascripts/index.js
+++ javascripts/index.js
@@ -6,7 +6,7 @@ require(["session_new"], function(sessionNew){
 $(document).ready(function(){
   $("#join_session").click(function(){
     document.cookie = "session_name=" + $("#input-name").val();
-    window.location.href = "session.html"
+    window.location.assign("http://hiveerli.me/poker/session.html");
   });
 });
 
