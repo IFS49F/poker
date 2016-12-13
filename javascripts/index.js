096fd9e26c5a86849c1fcd937d412f111c922b87
Hiveer <ljp.hiveer@gmail.com>
2016-12-13 18:42:40 +0800

    debug page location

--- javascripts/index.js
+++ javascripts/index.js
@@ -6,7 +6,7 @@ require(["session_new"], function(sessionNew){
 $(document).ready(function(){
   $("#join_session").click(function(){
     document.cookie = "session_name=" + $("#input-name").val();
-    window.location.assign("http://hiveerli.me/poker/session.html");
+    document.location.assign("http://hiveerli.me/poker/session.html");
   });
 });
 
