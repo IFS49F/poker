18e48b0c9ef8101475a4207f78a1c15ce1cc5676
Hiveer <ljp.hiveer@gmail.com>
2016-12-13 18:19:58 +0800

    fix coding error, typo of val() to value

--- javascripts/index.js
+++ javascripts/index.js
@@ -5,7 +5,7 @@ require(["session_new"], function(sessionNew){
 
 $(document).ready(function(){
   $("#join_session").click(function(){
-    document.cookie = "session_name=" + $("#input-name").value;
+    document.cookie = "session_name=" + $("#input-name").val();
     window.location.href = "session.html"
   });
 });
