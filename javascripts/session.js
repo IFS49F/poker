363e34fede3e6de0eaff1240cf55d2fb1853e443
Hiveer <ljp.hiveer@gmail.com>
2016-12-14 17:46:13 +0800

    don't use requirejs on a single page

--- javascripts/session.js
+++ javascripts/session.js
@@ -1,19 +1,17 @@
-define(["./../bower_components/js-cookie/src/js.cookie"], function(Cookies){
-  var sessionName = Cookies.get("session_name");
+var sessionName = Cookies.get("session_name");
 
-  $.ajax("https://leancloud.cn:443/1.1/classes/session",
-  {
-    headers: { "X-LC-Id": "IuBpRcjICs1OlVjLeBm99rSO-gzGzoHsz",
-              "X-LC-Key": "Tan5kGI0Swx4cMte10sHrEjW" },
-    data: 'where=' + JSON.stringify({"name":sessionName}),
-    success: function(data){
-      $("#session_name").text(sessionName);
-      var points = data.results[0].points;
-      for(var i = 0 ; i < points.length; i++) {
-        var row$ = $('<button type="button" class="btn btn-info">');
-        row$.html(points[i]);
-        $("#point_labels").append(row$);
-      }
+$.ajax("https://leancloud.cn:443/1.1/classes/session",
+{
+  headers: { "X-LC-Id": "IuBpRcjICs1OlVjLeBm99rSO-gzGzoHsz",
+            "X-LC-Key": "Tan5kGI0Swx4cMte10sHrEjW" },
+  data: 'where=' + JSON.stringify({"name":sessionName}),
+  success: function(data){
+    $("#session_name").text(sessionName);
+    var points = data.results[0].points;
+    for(var i = 0 ; i < points.length; i++) {
+      var row$ = $('<button type="button" class="btn btn-info">');
+      row$.html(points[i]);
+      $("#point_labels").append(row$);
     }
-  });
+  }
 });
