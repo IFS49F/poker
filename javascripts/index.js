74c8509d4a3027527aaabbf612a89d84ab2a26df
Hiveer <ljp.hiveer@gmail.com>
2016-12-15 10:25:30 +0800

    new page join_session

--- javascripts/index.js
+++ javascripts/index.js
@@ -5,7 +5,7 @@ require(["session_new"], function(sessionNew){
 $(document).ready(function(){
   $("#join_session").click(function(){
     document.cookie = "session_name=" + $("#input-name").val();
-    window.location.href = "http://hiveerli.me/poker/session.html";
+    window.location.href = "http://hiveerli.me/poker/join_session.html";
     return false;
   });
 });
