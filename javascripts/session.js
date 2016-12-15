d2ff939e1b65dd9def42c8043480e1275cc922bd
Hiveer <ljp.hiveer@gmail.com>
2016-12-15 17:05:56 +0800

    load default points data right now

--- javascripts/session.js
+++ javascripts/session.js
@@ -1,25 +1,46 @@
 var sessionName = Cookies.get("session_name");
 var userName = Cookies.get("user_name");
 
+var defaultPoints = [
+  {label: "0 points", value: "0"},
+  {label: "0.5 point", value: "0.5"},
+  {label: "1 point", value: "1"},
+  {label: "2 points", value: "2"},
+  {label: "3 points", value: "3"},
+  {label: "5 points", value: "5"},
+  {label: "8 points", value: "8"},
+  {label: "13 points", value: "13"},
+  {label: "20 points", value: "20"},
+  {label: "40 points", value: "40"},
+  {label: "100 points", value: "100"},
+  {label: "?", value: '?'}
+];
 // initialize websocket
 // make new conncetion
 var ws = new WebSocket('ws://achex.ca:4010');
 
-$.ajax("https://leancloud.cn:443/1.1/classes/session",
-{
-  headers: { "X-LC-Id": "IuBpRcjICs1OlVjLeBm99rSO-gzGzoHsz",
-            "X-LC-Key": "Tan5kGI0Swx4cMte10sHrEjW" },
-  data: 'where=' + JSON.stringify({"name":sessionName}),
-  success: function(data){
-    $("#session_name").text(sessionName);
-    var points = data.results[0].points;
-    for(var i = 0 ; i < points.length; i++) {
-      var row$ = $('<button type="button" class="btn btn-info points">');
-      row$.html(points[i].label);
-      $("#point_labels").append(row$);
-    }
-  }
-});
+// $.ajax("https://leancloud.cn:443/1.1/classes/session",
+// {
+  // headers: { "X-LC-Id": "IuBpRcjICs1OlVjLeBm99rSO-gzGzoHsz",
+            // "X-LC-Key": "Tan5kGI0Swx4cMte10sHrEjW" },
+  // data: 'where=' + JSON.stringify({"name":sessionName}),
+  // success: function(data){
+    // $("#session_name").text(sessionName);
+    // var points = data.results[0].points;
+    // for(var i = 0 ; i < points.length; i++) {
+      // var row$ = $('<button type="button" class="btn btn-info points">');
+      // row$.html(points[i].label);
+      // $("#point_labels").append(row$);
+    // }
+  // }
+// });
+
+
+for(var i = 0 ; i < defaultPoints.length; i++) {
+  var row$ = $('<button type="button" class="btn btn-info points">');
+  row$.html(defaultPoints[i].label);
+  $("#point_labels").append(row$);
+}
 
 // show user on page
 var user_point$ = $('<tr>');
