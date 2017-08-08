5c5cbac93a8776f3552c930e699f20619eea9b23
James Shih <gtx1024@gmail.com>
2017-08-08 17:44:22 +0800

    Add Google Analytics

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import App from 'pages/App/App';
 
 import registerServiceWorker from './registerServiceWorker';
+import googleAnalytics from './googleAnalytics';
 import './index.css';
+
+googleAnalytics();
 
 ReactDOM.render(
   <App />,
