fbbb97bf04d055177cab4268a6b7027c7fa68bea
just4fun <houritsunohikari@gmail.com>
2017-08-19 17:48:11 +0800

    add component for github-corner

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Landing from 'pages/Landing/Landing';
 import Room from 'pages/Room/Room';
+import GithubCorner from 'components/GithubCorner/GithubCorner';
 import './App.css';
 
 const App = () => (
@@ -9,6 +10,7 @@ const App = () => (
     <div>
       <Route exact path="/" component={Landing} />
       <Route path="/:room" component={Room} />
+      <GithubCorner url='https://github.com/IFS49F' />
     </div>
   </Router>
 );
