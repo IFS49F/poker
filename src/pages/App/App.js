0af6692a52247e3a1cbc7febd3d5760f20093822
James Shih <gtx1024@gmail.com>
2017-08-27 12:28:15 +0800

    Revert "add component for github-corner"
    This reverts commit fbbb97bf04d055177cab4268a6b7027c7fa68bea.

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Landing from 'pages/Landing/Landing';
 import Room from 'pages/Room/Room';
-import GithubCorner from 'components/GithubCorner/GithubCorner';
 import './App.css';
 
 const App = () => (
@@ -10,7 +9,6 @@ const App = () => (
     <div>
       <Route exact path="/" component={Landing} />
       <Route path="/:room" component={Room} />
-      <GithubCorner url='https://github.com/IFS49F' />
     </div>
   </Router>
 );
