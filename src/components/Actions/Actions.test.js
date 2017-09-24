8870bdfd11fca91fc8c6e282126de24b1c7a68f9
just4fun <houritsunohikari@gmail.com>
2017-09-24 01:39:15 -0500

    test: remove chai, since built-in `expect` from Jest is enough

--- src/components/Actions/Actions.test.js
+++ src/components/Actions/Actions.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import { expect } from 'chai';
 import Actions from './Actions';
 
 describe('<Actions />', () => {
@@ -11,23 +10,23 @@ describe('<Actions />', () => {
 
     it('should render 14 buttons for valid scores', () => {
       const wrapper = shallow(<Actions />);
-      expect(wrapper.find('.scores button')).to.have.length(14);
+      expect(wrapper.find('.scores button')).toHaveLength(14);
     });
 
     it('should know which score has been voted', () => {
       const wrapper = shallow(<Actions myScore={8} />);
-      expect(wrapper.find('.scores button.selected').text()).to.equal('8');
+      expect(wrapper.find('.scores button.selected').text()).toEqual('8');
     });
 
     it('should render a `Show` button when `show` is `false`', () => {
       const wrapper = shallow(<Actions show={false} />);
-      expect(wrapper.find('.operations button').text()).to.equal('Show');
+      expect(wrapper.find('.operations button').text()).toEqual('Show');
     });
 
     it('should render a `Clear` button with proper CSS class when `show` is `false`', () => {
       const wrapper = shallow(<Actions show={true} />);
-      expect(wrapper.find('.operations button').text()).to.equal('Clear');
-      expect(wrapper.find('.operations button').hasClass('danger')).to.be.true;
+      expect(wrapper.find('.operations button').text()).toEqual('Clear');
+      expect(wrapper.find('.operations button').hasClass('danger')).toEqual(true);
     });
   });
 
@@ -45,9 +44,9 @@ describe('<Actions />', () => {
     });
 
     it('requires necessary action props', () => {
-      expect(wrapper.props().onVote).to.exist;
-      expect(wrapper.props().onShow).to.exist;
-      expect(wrapper.props().onClear).to.exist;
+      expect(wrapper.props().onVote).toBeDefined;
+      expect(wrapper.props().onShow).toBeDefined;
+      expect(wrapper.props().onClear).toBeDefined;
     });
   });
 });
--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { expect } from 'chai';
 import Card from './Card';
 
 describe('<Card />', () => {
@@ -11,24 +10,24 @@ describe('<Card />', () => {
 
     it('should render 2 faces', () => {
       const wrapper = shallow(<Card />);
-      expect(wrapper.find('.Card .face')).to.have.length(2);
+      expect(wrapper.find('.Card .face')).toHaveLength(2);
     });
 
     it('should render Card with proper CSS class if `voted` is `true`', () => {
       const wrapper = shallow(<Card voted={true} />);
-      expect(wrapper.find('.Card').hasClass('voted')).to.be.true;
+      expect(wrapper.find('.Card').hasClass('voted')).toEqual(true);
     });
 
     it('should render Card with proper CSS class if `show` is `true`', () => {
       const wrapper = shallow(<Card show={true} />);
-      expect(wrapper.find('.Card').hasClass('show')).to.be.true;
+      expect(wrapper.find('.Card').hasClass('show')).toEqual(true);
     });
 
     it('should render Card with proper score', () => {
       let wrapper = shallow(<Card />);
-      expect(wrapper.find('.front').text()).to.equal('');
+      expect(wrapper.find('.front').text()).toEqual('');
       wrapper = shallow(<Card show={true} voted={true} score={13} />);
-      expect(wrapper.find('.front').text()).to.equal('13');
+      expect(wrapper.find('.front').text()).toEqual('13');
     });
   });
 });
