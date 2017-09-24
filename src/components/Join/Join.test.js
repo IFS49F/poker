629e5b51d7073d51d8a96e632ba865a786087e74
just4fun <houritsunohikari@gmail.com>
2017-09-24 01:35:21 -0500

    test: add unit tests for <Join />

--- src/components/Join/Join.test.js
+++ src/components/Join/Join.test.js
@@ -1,8 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
 import Join from './Join';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Join />, div);
+describe('<Join />', () => {
+  describe('Structures', () => {
+    it('should render without crashing', () => {
+      shallow(<Join />);
+    });
+
+    it('should render an input field', () => {
+      const wrapper = shallow(<Join />);
+      expect(wrapper.find('input[type="text"]')).toHaveLength(1);
+    });
+
+    it('should render a Play button', () => {
+      const wrapper = shallow(<Join />);
+      expect(wrapper.find('button[type="submit"]').text()).toEqual('Play');
+    });
+  });
+
+  describe('Behaviors', () => {
+    let onSubmit;
+    let wrapper;
+
+    onSubmit = jest.fn();
+    wrapper = mount(<Join onSubmit={onSubmit} playerName={''} />);
+
+    it('requires necessary action props', () => {
+      expect(wrapper.props().onSubmit).toBeDefined;
+    });
+
+    it('should call `onSubmit` if submit form', () => {
+      const form = wrapper.find('form').first();
+      const input = wrapper.find('input[type="text"]').first();
+      input.simulate('change', {
+        target: {
+          name: 'myName',
+          value: 'just4fun'
+        }
+      });
+      form.simulate('submit');
+      expect(onSubmit).toBeCalledWith('just4fun');
+    });
+  });
 });
