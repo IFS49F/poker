a35e46c676e5fdc643cb20a7683e9fb797476a2a
just4fun <houritsunohikari@gmail.com>
2019-07-13 00:05:06 +0800

    feat: mark expiry date as red color if it's close within 30 days

--- src/pages/Room/Room.js
+++ src/pages/Room/Room.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import classNames from 'classnames';
 import Share from 'components/Share/Share';
 import Notification from 'components/Notification/Notification';
 import Join from 'components/Join/Join';
@@ -168,6 +169,13 @@ class Room extends Component {
   render() {
     const { me, myScore, highlightScore, team, playerAction, show, disconnected, reconnCountdown } = this.state;
     const playerName = localStorage.getItem('playerName') || '';
+    const expiryDate = new Date(process.env.REACT_APP_DOMAIN_EXPIRY_DATE);
+    const today = new Date();
+    const isLessThanOneMonth = ((expiryDate.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)) < 30
+    const expiryDateClasses = classNames({
+      'expiry-date': true,
+      'expiry-date--close': isLessThanOneMonth
+    });
 
     return (
       <div className="Room">
@@ -179,7 +187,7 @@ class Room extends Component {
             Poker4Fun costs US $60 for server and US $30 for domain every year.
           </p>
           <p>
-            Currently the domain is available until <span className="renew-date">{process.env.REACT_APP_DOMAIN_EXPIRY_DATE}</span>.
+            Currently the domain is available until <span className={expiryDateClasses}>{process.env.REACT_APP_DOMAIN_EXPIRY_DATE}</span>.
           </p>
           <p>
             Please click <a href="https://github.com/IFS49F/poker#donate" target="_blank" rel="noopener noreferrer">here</a> to help us keeping Poker4Fun free and sustainable.
